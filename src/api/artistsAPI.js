ab38fdbffecabe801d30b807dca009d8afc113c5
unknown <m.razajamil@gmail.com>
2020-03-09 02:15:25 +1100

    changed api urls to https to fix google app engine errors

--- src/api/artistsAPI.js
+++ src/api/artistsAPI.js
@@ -5,12 +5,12 @@ const responseFormat = 'json'
 
 export const getTopArtistsByCountry = ({ country, page, resultsPerPage }) => {
   return Axios.get(
-    `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&api_key=${api_config.api_key}&country=${country}&page=${page}&limit=${resultsPerPage}&format=${responseFormat}`
+    `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&api_key=${api_config.api_key}&country=${country}&page=${page}&limit=${resultsPerPage}&format=${responseFormat}`
   )
 }
 
 export const getArtistTopTracks = ({ artistID }) => {
   return Axios.get(
-    `http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&mbid=${artistID}&page=1&limit=5&api_key=${api_config.api_key}&format=${responseFormat}`
+    `https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&mbid=${artistID}&page=1&limit=5&api_key=${api_config.api_key}&format=${responseFormat}`
   )
 }
