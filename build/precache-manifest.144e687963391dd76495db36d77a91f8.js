8573d5ec52d295366ed8f8a26e47c41b0b916218
unknown <m.razajamil@gmail.com>
2020-03-09 01:46:52 +1100

    change dev server port to 8080

---
+++ build/precache-manifest.144e687963391dd76495db36d77a91f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15958e31f1c537361280c2e4919e5611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e586a657b22d2faa95a",
+    "url": "/static/css/main.3356bca1.chunk.css"
+  },
+  {
+    "revision": "abef500b38e0583c81de",
+    "url": "/static/js/2.7a630933.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.7a630933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e586a657b22d2faa95a",
+    "url": "/static/js/main.577f8cc2.chunk.js"
+  },
+  {
+    "revision": "d79c7d4849591b45e0f1",
+    "url": "/static/js/runtime-main.3e679f8c.js"
+  }
+]);
